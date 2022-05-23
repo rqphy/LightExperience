c064fe49b27d99dadae957f373d550706efa17de
rqphy <raphael.ferreira@hetic.net>
2022-05-24 00:00:54 +0200

    :wastebasket: cleanin

--- src/Experience/CameraAnimation.js
+++ src/Experience/CameraAnimation.js
@@ -14,19 +14,6 @@ export default class CameraAnimation
         this.btns = []
 
         this.setListeners()
-
-        // Front
-        // x: 0
-        // y: 0
-        // z: 8
-        // Side
-        // x: -5.533833711541049
-        // y: 1.4644913989102093
-        // z: -3.1130141339266837
-        // Top
-        // x: -1.6056395550286033
-        // y: 7.690507651664971
-        // z: -1.5093090071262074
     }
 
     setListeners()
@@ -43,7 +30,6 @@ export default class CameraAnimation
 
     handleViewClick(view)
     {
-        console.log(view)
         let coords = {}
         switch (view) {
             case 'side':
@@ -64,13 +50,11 @@ export default class CameraAnimation
                 break
         }
 
-        console.log(coords)
         this.animateCamera(coords)
     }
 
     animateCamera(position)
     {
-        // console.log(this.camera)
         new TWEEN.Tween(this.camera.instance.position)
             .to(position, 1800)
             .easing(TWEEN.Easing.Quadratic.InOut)
